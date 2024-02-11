7d13684cf412a110e1b870641e5b90193b1fe43e
SusiAT <132758488+SusiAT@users.noreply.github.com>
2024-02-11 10:13:40 -0500

    Add files via upload

--- Pareto/Pareto resumido.py
+++ Pareto/Pareto resumido.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import os
 
 # Directorio que contiene los archivos CSV
-csv_directory = "C:/programa/Pareto"
+csv_directory = "D:/programa/Pareto"
 
 # Obtener la lista de archivos CSV en el directorio
 csv_files = [f for f in os.listdir(csv_directory) if f.endswith('.csv')]
@@ -14,17 +14,17 @@ df_pareto = pd.concat([pd.read_csv(os.path.join(csv_directory, file), sep=';') f
 
 # Guardar las primeras 100 líneas del DataFrame combinado en un archivo CSV llamado Pareto.csv
 # Especificar el separador de decimales en la columna Percent como '.'
-df_pareto.head(100).to_csv("Pareto.csv", index=False, sep=';', decimal='.')
+df_pareto.to_csv("Pareto.csv", index=False, sep=';', decimal='.')
 
 # Proceso para obtener ResumenPareto.csv
 # Guardar las primeras 100 líneas del DataFrame combinado en un archivo CSV llamado ResumenPareto.csv
 # Especificar el separador de decimales en la columna Percent como '.'
-df_pareto.head(100).to_csv("ResumenPareto.csv", index=False, sep=';', decimal='.')
+df_pareto.head(587804).to_csv("ResumenPareto.csv", index=False, sep=';', decimal='.')
 
 # Calcular el Pareto utilizando ResumenPareto.csv
 # Supongamos que tienes una columna llamada "cantidad" que representa la frecuencia de cada categoría.
 # Ajusta la columna según tus datos.
-df_pareto_sorted = df_pareto.head(100).sort_values(by='Percent', ascending=False)
+df_pareto_sorted = df_pareto.head(587804).sort_values(by='Percent', ascending=False)
 df_pareto_sorted['cumulative_percentage'] = df_pareto_sorted['Percent'].cumsum() / df_pareto_sorted['Percent'].sum() * 100
 
 # Graficar el resultado de manera elegante
